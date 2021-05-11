a6e46d7e5eaad85a1c9161613072e37e0d2dff3b
Michael Graves <mgrav@tutanota.com>
2021-05-11 17:26:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pygame
+
+def create_board():
+    board = np.zeros((6,7));
+    return board
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 input
+    if turn == 0:
+        selection = int(input("Player 1 Make your Selection (0-6): "))
+
+        print(selection)
+        print(type(selection))
+
+    # Ask for Player 2 input
