6e79dfb7c504e21f68da944616035511cd950c46
Michael Graves <mgrav@tutanota.com>
2021-05-11 17:35:05 +0000

    Added new methods to check if the move is valid

--- main.py
+++ main.py
@@ -5,21 +5,31 @@ def create_board():
     board = np.zeros((6,7));
     return board
 
+def drop_piece():
+    pass
+
+def is_valid_location(board, col):
+    pass
+
+def get_next_open_row():
+    pass
+
 board = create_board()
+print(board)
 game_over = False
 turn = 0
 
 while not game_over:
     # Ask for Player 1 input
+    # User inputs the column they want to drop in
     if turn == 0:
-        selection = int(input("Player 1 Make your Selection (0-6): "))
+        col = int(input("Player 1 Make your Selection (0-6): "))
 
-        print(selection)
-        print(type(selection))
 
     # Ask for Player 2 input
     else:
-        selection = int(input("Player 2 Make your Selection (0-6): "))
+        col = int(input("Player 2 Make your Selection (0-6): "))
 
+    # Makes sure the turn is properly updated to switch back and forth
     turn += 1
     turn = turn % 2
